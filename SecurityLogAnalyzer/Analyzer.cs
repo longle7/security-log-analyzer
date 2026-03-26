e1d4a503874d336b6fbda76cfea8855cc7f6587f
longle7 <longhuynhle1@hotmail.com>
2026-03-26 13:06:03 -0400

    v1.2 added comments to Analyzer.cs before implementing PLINQ

--- SecurityLogAnalyzer/Analyzer.cs
+++ SecurityLogAnalyzer/Analyzer.cs
@@ -13,13 +13,26 @@ public class Analyzer
         // Starting timer
         var sw = System.Diagnostics.Stopwatch.StartNew();
 
+        // Reading the file that contains log information - each log entry has the properties as the LogEntry class
         string json = File.ReadAllText("logs.json");
+
+        // logs is a list of indiv log entry from the json provided
         var logs = JsonSerializer.Deserialize<List<LogEntry>>(json)!;
 
+        // failedLogins contains failed logins where the event in the current log == "failed_login"
         var failedLogins = logs.Where(l => l.Event == "failed_login").ToList();
+
+        // groups the failed logins by IP address - well have about 254 IGrouping<string, LogEntry> objects
+        // EX:  Group 1: Key="192.168.1.42" → 847 failed logins from this IP
+
+        // the .Select takes each raw group and transforms it into a clean object:
+        // for each IP in the in the .GroupBy sort the timestamps oldest -> newest
+        // EX:  ipGroups[0] = { Ip: "192.168.1.42", Events: [10:00am, 10:02am, 10:05am, ..., 10:23am] }
+        //      ipGroups[1] = { Ip: "192.168.1.99", Events: [9:45am, 9:47am, ..., 11:02am] }
         var ipGroups = failedLogins.GroupBy(l => l.Ip)
                                   .Select(g => new { Ip = g.Key, Events = g.OrderBy(l => l.Timestamp).ToList() });
 
+        // One thread and is slow
         var alerts = new List<string>();
         foreach (var group in ipGroups)
         {
